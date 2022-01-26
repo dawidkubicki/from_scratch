6b9d21b284d92f0b87d0a00c18bc330385621e05
Dawid <dawid@pluscode.io>
2022-01-26 13:37:59 +0100

    minor changes

--- perceptron/numpy/main.py
+++ perceptron/numpy/main.py
@@ -9,6 +9,7 @@ y = y.astype(int)
 print(f"X.shape: {X.shape}")
 print(f"y.shape: {y.shape}")
 
+
 # Shuffling dataset
 shuff_idx = np.arange(y.shape[0])
 shuff_gen = np.random.RandomState(50)
@@ -19,6 +20,10 @@ X, y = X[shuff_idx], y[shuff_idx]
 X_train, X_test = X[shuff_idx[:70]], X[shuff_idx[70:]]
 y_train, y_test = y[shuff_idx[:70]], y[shuff_idx[70:]]
 
+# Normalize
+mu, sigma = X_train.mean(axis=0), X_train.std(axis=0)
+X_train = (X_train - mu) / sigma
+X_test = (X_test - mu) / sigma
 
 class Perceptron():
 	def __init__(self, num_features=2, epochs=50):
@@ -28,6 +33,15 @@ class Perceptron():
 		self.bias = np.zeros(1, dtype=float)
 
 	def plotter(self, X, y):
+		x0_min = np.amin(X[:1])
+		x1_min = ((-(x0_min*self.weight[0])-self.bias[0]) / self.weight[1]) 
+
+		x0_max = np.amax(X[:1])
+		x1_max = ((-(x0_max*self.weight[0])-self.bias[0] / self.weight[1]) 
+
+	
+			
+		plt.plot([x0_min, x0_max], [x1_min, x1_max])		
 		plt.scatter(X[y==0, 0], X[y==0, 1], label="Class A")
 		plt.scatter(X[y==1, 0], X[y==1, 1], label="Class A")
 		plt.title("Dataset")
@@ -35,9 +49,6 @@ class Perceptron():
 		plt.ylabel("Feature x2")
 		plt.legend()
 		plt.show()
-
-	
-	
 
 	def forward(self, X):
 		linear = np.dot(X, self.weights) + self.bias
@@ -61,10 +72,8 @@ class Perceptron():
 		predictions = self.forward(X).reshape(-1)
 		# Calculating accuracy -> sum where predictions are equal to real values, then divided by the amount of them
 		accuracy = np.sum(predictions==y) / y.shape[0]
-		print(f"Accuracy of model: {accuracy*100}%")
-
 
 perceptron = Perceptron(2, 50)
 perceptron.train(X_train, y_train)
-perceptron.plotter(X_train, y_train)
 perceptron.evaluate(X_test, y_test)
+perceptron.plotter(X_train, y_train)
